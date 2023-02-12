b4a980f9bc2b219bdcbbb7aace2a5e50db6174f5
AytajVV <aytajvv@code.edu.az>
2023-02-12 14:27:56 +0400

    js-intro

---
+++ assets/js/style.js
@@ -0,0 +1,23 @@
+let num=prompt();
+if(num%21==0){
+    console.log("Bolunur");
+}
+else{
+    console.log("Bolunmur")
+}
+
+
+
+//Task2
+
+let  n=parseInt(prompt());
+let m=parseInt(prompt());
+if(n%2==0 && m%2==0){
+    let x=n+m;
+    console.log(x);
+}
+else{
+    console.log(`${n} ve ${m} ededi cut deyil`);
+}
+
+
